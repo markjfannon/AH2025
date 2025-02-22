8345be60dc3ff145a2a1ac57158885f5cb1ce8ff
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2025-02-22 19:18:18 +0000

    Add file paths from file picker to queue

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,26 +1,26 @@
 use tauri::ipc::Response;
 
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![read_file])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![read_file])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn read_file(file_path : &str) -> Response {
-  let data = std::fs::read(file_path).unwrap();
-  tauri::ipc::Response::new(data)
+fn read_file(file_path: &str) -> Response {
+    let data = std::fs::read(file_path).unwrap();
+    tauri::ipc::Response::new(data)
 }
