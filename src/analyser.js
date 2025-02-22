4b8e13a152d5a2ad7d8360d16b896ad2dad56997
Alice Gowler <psyag8@nottingham.ac.uk>
2025-02-22 17:40:00 +0000

    Basic mic visualisation

---
+++ src/analyser.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.addEventListener("click", init);
+});
+
+async function init() {
+  const audioCtx = new AudioContext();
+  let source;
+  let stream;
+
+  // Set up the different audio nodes we will use for the app
+  const analyser = audioCtx.createAnalyser();
+  analyser.minDecibels = -90;
+  analyser.maxDecibels = -10;
+  analyser.smoothingTimeConstant = 0.85;
+
+  // Set up canvas context for visualizer
+  const canvas = document.getElementById("visualizer");
+  const canvasCtx = canvas.getContext("2d");
+
+  // Adjust canvas size based on container width
+  const intendedWidth = document.getElementById("box-two").clientWidth;
+  canvas.setAttribute("width", intendedWidth);
+
+  // Main block for doing the audio recording
+  const constraints = { audio: true };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      source = audioCtx.createMediaStreamSource(stream);
+      source.connect(analyser);
+
+      visualize(); // Start visualizing audio
+    })
+    .catch(function (err) {
+      console.error("The following gUM error occurred: " + err);
+    });
+
+  function visualize() {
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.fftSize;
+    const dataArray = new Uint8Array(bufferLength);
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    const draw = () => {
+      requestAnimationFrame(draw);
+
+      analyser.getByteTimeDomainData(dataArray);
+
+      canvasCtx.fillStyle = "rgb(200, 200, 200)";
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+      canvasCtx.beginPath();
+
+      const sliceWidth = (WIDTH * 1.0) / bufferLength;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0;
+        const y = (v * HEIGHT) / 2;
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(WIDTH, HEIGHT / 2);
+      canvasCtx.stroke();
+    };
+
+    draw();
+  }
+}
