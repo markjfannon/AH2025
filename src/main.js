6059e42432abffab426e4167f574069725975b91
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2025-02-22 20:59:54 +0000

    Add file filtering and export add and remove song functions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import "./style.css";
-import { setupQueue } from "./queue";
 import { init } from "./analyser";
+import { setupQueue } from "./queue";
+import "./style.css";
 
-setupQueue(document.querySelector("#queue"));
+const { addSong, removeSong } = setupQueue(document.querySelector("#queue"));
 document.getElementById("play-button").addEventListener("click", init);
