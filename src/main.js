a27866e018d9dbff896f5153f2123212a6806d47
larleevee <lanavalentine1889@gmail.com>
2025-02-23 00:09:45 +0000

    added cat follow

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { init } from "./analyser";
 import { setupQueue } from "./queue";
+import { initOneko } from "./oneko"; // Import Oneko
 import "./style.css";
 
 const { addSong, removeSong } = setupQueue(document.querySelector("#queue"));
 document.getElementById("play-button").addEventListener("click", init);
+
+initOneko();
+
